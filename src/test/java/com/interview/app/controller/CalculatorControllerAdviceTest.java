41a0af9d2c189c0100635fa59d7860ba807533f1
afro105 <afro105@wp.pl>
2022-02-02 20:16:46 +0100

    UTs for CalculatorControllerAdvice has been added

---
+++ src/test/java/com/interview/app/controller/CalculatorControllerAdviceTest.java
@@ -0,0 +1,88 @@
+package com.interview.app.controller;
+
+import com.interview.app.dto.CalculationResult;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.ConstraintViolationException;
+import java.util.Set;
+
+import static com.interview.app.controller.CalculatorControllerAdvice.VALIDATION_ERROR_DEFAULT_MESSAGE;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+class CalculatorControllerAdviceTest {
+
+    private final CalculatorControllerAdvice calculatorControllerAdvice = new CalculatorControllerAdvice();
+
+    @Test
+    void handleIllegalArgumentException_ShouldReturnExceptionMessage() {
+        //given
+        String exceptionMessage = "Wrong data";
+        IllegalArgumentException exception = new IllegalArgumentException(exceptionMessage);
+
+        //when
+        ResponseEntity<CalculationResult> responseEntity = calculatorControllerAdvice.handleIllegalArgumentException(exception);
+
+        //then
+        assertThat(responseEntity).isNotNull();
+        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.BAD_REQUEST);
+
+        CalculationResult body = responseEntity.getBody();
+        assertThat(body).isNotNull();
+        assertThat(body.getMessage()).isEqualTo(exceptionMessage);
+        assertThat(body.getError()).isTrue();
+        assertThat(body.getResult()).isNull();
+    }
+
+    @Test
+    void handleValidationException_ShouldReturnDefMessageWhenNoConstraints() {
+        //given
+        ConstraintViolationException exception = new ConstraintViolationException(Set.of());
+
+        //when
+        ResponseEntity<CalculationResult> responseEntity = calculatorControllerAdvice.handleValidationException(exception);
+
+        //then
+        assertThat(responseEntity).isNotNull();
+        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.BAD_REQUEST);
+
+        CalculationResult body = responseEntity.getBody();
+        assertThat(body).isNotNull();
+        assertThat(body.getMessage()).isEqualTo(VALIDATION_ERROR_DEFAULT_MESSAGE);
+        assertThat(body.getError()).isTrue();
+        assertThat(body.getResult()).isNull();
+    }
+
+    @Test
+    void handleValidationException_ShouldReturnFirstConstraintMessage() {
+        //given
+        String exceptionMessage1 = "Wrong parameter 1";
+        ConstraintViolation<String> constraintViolation1 = mock(ConstraintViolation.class);
+        when(constraintViolation1.getMessage()).thenReturn(exceptionMessage1);
+
+        String exceptionMessage2 = "Wrong parameter 2";
+        ConstraintViolation<String> constraintViolation2 = mock(ConstraintViolation.class);
+        when(constraintViolation2.getMessage()).thenReturn(exceptionMessage2);
+
+
+        ConstraintViolationException exception = new ConstraintViolationException(Set.of(constraintViolation1, constraintViolation2));
+
+        //when
+        ResponseEntity<CalculationResult> responseEntity = calculatorControllerAdvice.handleValidationException(exception);
+
+        //then
+        assertThat(responseEntity).isNotNull();
+        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.BAD_REQUEST);
+
+        CalculationResult body = responseEntity.getBody();
+        assertThat(body).isNotNull();
+        assertThat(body.getMessage()).isEqualTo(exceptionMessage1);
+        assertThat(body.getError()).isTrue();
+        assertThat(body.getResult()).isNull();
+    }
+
+}
