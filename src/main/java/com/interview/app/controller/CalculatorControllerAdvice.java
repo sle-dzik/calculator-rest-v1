2844e7d2a624442184408d67058995c2b7a4c45f
afro105 <afro105@wp.pl>
2022-02-02 19:28:32 +0100

    UTs for CalculatorControllerAdvice has been added

--- src/main/java/com/interview/app/controller/CalculatorControllerAdvice.java
+++ src/main/java/com/interview/app/controller/CalculatorControllerAdvice.java
@@ -7,21 +7,31 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+import javax.validation.ConstraintViolation;
 import javax.validation.ConstraintViolationException;
 
 @ControllerAdvice
 public class CalculatorControllerAdvice extends ResponseEntityExceptionHandler {
 
+    public static String VALIDATION_ERROR_DEFAULT_MESSAGE = "Validation error";
+
     @ExceptionHandler(value = {IllegalArgumentException.class})
     protected ResponseEntity<CalculationResult> handleIllegalArgumentException(IllegalArgumentException ex) {
-        CalculationResult build = CalculationResult.builder().message(ex.getMessage()).build();
-        return new ResponseEntity<>(build, HttpStatus.BAD_REQUEST);
+        CalculationResult build = buildErrorResponse(ex.getMessage());
+        return new ResponseEntity<>(buildErrorResponse(ex.getMessage()), HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(ConstraintViolationException.class)
-    public ResponseEntity<CalculationResult> handleValidationExceptions(ConstraintViolationException ex) {
-        String message = ex.getConstraintViolations().stream().findFirst().get().getMessage();
-        CalculationResult build = CalculationResult.builder().message(message).build();
-        return new ResponseEntity<>(build, HttpStatus.BAD_REQUEST);
+    public ResponseEntity<CalculationResult> handleValidationException(ConstraintViolationException ex) {
+        String message = ex.getConstraintViolations().stream()
+                .findFirst()
+                .map(ConstraintViolation::getMessage)
+                .orElse(VALIDATION_ERROR_DEFAULT_MESSAGE);
+
+        return new ResponseEntity<>(buildErrorResponse(message), HttpStatus.BAD_REQUEST);
+    }
+
+    private CalculationResult buildErrorResponse(String message) {
+        return CalculationResult.builder().error(true).message(message).build();
     }
 }
--- src/main/java/com/interview/app/dto/CalculationResult.java
+++ src/main/java/com/interview/app/dto/CalculationResult.java
@@ -1,13 +1,16 @@
 package com.interview.app.dto;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Builder;
 import lombok.Getter;
 
 @Getter
 @Builder
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class CalculationResult {
 
-    private boolean error;
+    private Boolean error;
     private Double result;
     private String message;
+
 }
